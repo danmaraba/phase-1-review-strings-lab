27ff143f4b69c88e2931a5083913e6dc35e5e253
Daniel Kipruto Maraba <danielkmaraba@gmail.com>
2022-06-06 15:52:43 +0300

    Done with the assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser='Daniel Kipruto'
+const welcomeMessage='Welcome to Flatbook, Daniel Kipruto!'
+const excitedWelcomeMessage='WELCOME TO FLATBOOK, DANIEL KIPRUTO!'
+const shortGreeting='Welcome, D!'
