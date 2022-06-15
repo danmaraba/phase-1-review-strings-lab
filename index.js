20ec8837d94f497c6ebd54b5fd49896daf0c8880
Daniel Kipruto Maraba <danielkmaraba@gmail.com>
2022-06-15 05:15:52 +0300

    commit tree e4d20c2598d9f67276ac3676c2c4e26d689a960d

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 // Write your code in this file!
-const currentUser='Daniel Kipruto'
-const welcomeMessage='Welcome to Flatbook, Daniel Kipruto!'
-const excitedWelcomeMessage='WELCOME TO FLATBOOK, DANIEL KIPRUTO!'
-const shortGreeting='Welcome, D!'
+// const currentUser='Daniel Kipruto'
+// const welcomeMessage='Welcome to Flatbook, Daniel Kipruto!'
+// const excitedWelcomeMessage='WELCOME TO FLATBOOK, DANIEL KIPRUTO!'
+// const shortGreeting='Welcome, D!'
+
+
+let currentUser = 'Daniel Kipruto';
+currentUser = currentUser.toUpperCase();
+const welcomeMessage = 'Welcome to Flatbook, '+currentUser+'!';
+
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " +currentUser + "!"
+
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
